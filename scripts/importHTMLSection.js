cf5dce4e59658ffaa3b6347de8fa242531306398
Kenji195 <101021987+Kenji195@users.noreply.github.com>
2026-04-11 13:18:46 -0700

    Recent changes

---
+++ scripts/importHTMLSection.js
@@ -0,0 +1,21 @@
+class IncludeHTML extends HTMLElement {
+  connectedCallback() {
+    const filename = this.getAttribute('section');
+
+    if (!filename) {
+      return;
+    }
+
+    fetch('/html/sections/' + filename + '.html')
+      .then(res => res.text())
+      .then(data => {
+        this.innerHTML = data;
+      })
+      .catch(err => {
+        this.innerHTML = 'Error loading content.';
+        console.error(err);
+      });
+  }
+}
+
+customElements.define('include-html', IncludeHTML);
