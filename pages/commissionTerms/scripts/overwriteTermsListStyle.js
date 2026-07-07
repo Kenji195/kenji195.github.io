0b433046ea3e47ba84aab3f454d0090130bd5be9
Kenji195 <101021987+Kenji195@users.noreply.github.com>
2026-07-07 12:43:23 -0700

    Commissions updated

--- pages/commissionTerms/scripts/overwriteTermsListStyle.js
+++ pages/commissionTerms/scripts/overwriteTermsListStyle.js
@@ -7,3 +7,18 @@ el.innerHTML = el.innerHTML.replace(
   regex,
   '<span class="bolderText">$1</span>',
 );
+
+const charel = document.getElementById('character-section');
+const charTermsToBold = [
+  'Character Displays',
+  'Character Display',
+  'Characters',
+  'Character',
+];
+
+const charRegex = new RegExp(`\\b(${charTermsToBold.join('|')})\\b`, 'g');
+
+charel.innerHTML = charel.innerHTML.replace(
+  charRegex,
+  '<span class="bolderText">$1</span>',
+);
